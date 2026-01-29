3687dc7e3cdd3cd4567c9920170efdd376e7315a
arunsappa6 <arun.sappa@gmail.com>
2026-01-28 19:21:42 -0500

    New Code is added

--- top_customers.sql
+++ top_customers.sql
@@ -1,4 +1,5 @@
 -- Query to find top 5 customers by total spend
+/*
 SELECT 
     customer_id, 
     SUM(order_value) AS total_spend
@@ -6,3 +7,9 @@ FROM orders
 GROUP BY customer_id
 ORDER BY total_spend DESC
 LIMIT 5;
+*/
+
+SELECT customer_id, count(*) as row_cust_cnt
+from orders
+group by customer_id
+order by row_cust_cnt DESC;
